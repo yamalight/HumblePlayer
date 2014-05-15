96041e29e3d73b0be52a6741cd05c48b43f0a806
Tim Ermilov <yamalight@gmail.com>
2014-05-15 18:59:40 +0200

    minify js

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var header = require('gulp-header');
+var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 module.exports = function() {
     return gulp.src('./src/js/index.js')
         .pipe(browserify())
+        .pipe(uglify({mangle: false}))
         .pipe(header('nodeRequire = require; '))
         .pipe(rename('app.min.js'))
         .pipe(gulp.dest('./app/dist/'));
